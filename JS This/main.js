9400073e6bf9046c31aea4b7fee47a7b29927f9b
Chikoom <callforcopy@gmail.com>
2020-06-21 13:21:52 +0300

    JS This basic files and folders

---
+++ JS This/main.js
@@ -0,0 +1,13 @@
+// Exercise 1
+exDivider(1);
+
+
+
+function exDivider(number) {
+  console.log(" ");
+  console.log(" ");
+  console.log("==========================");
+  console.log("Exercise " + number);
+  console.log("==========================");
+  console.log(" ");
+}
